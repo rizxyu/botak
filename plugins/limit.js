f25e40c6deb5ecaab4b5fce2ff218efa51042e74
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-04 18:39:52 +0700

    Update limit.js

--- plugins/limit.js
+++ plugins/limit.js
@@ -2,7 +2,7 @@ let handler = async (m) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
-    m.reply(`Limit: ${global.DATBASE.data.users[who].limit} ಥ_ಥ`)
+    m.reply(`Limit: ${global.DATABASE.data.users[who].limit} ಥ_ಥ`)
 }
 handler.help = ['limit [@user]']
 handler.tags = ['xp']
