8443879c707c0aa29a297be5ae1ac34264c70ca8
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-04 14:27:53 +0700

    Add files via upload

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,53 +1,45 @@
-let fetch = require('node-fetch')
+let axios = require('axios')
 
-let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Uhm...url nya mana?'
-  let res = await fetch(global.API('xteam', '/dl/fb', {
-    url: args[0]
-  }, 'APIKEY'))
-  if (res.status !== 200) {
-    res.text()
-    throw res.status
-  }
-  let json = await res.json()
-  if (!json.result) throw json
-  let { name, author, description, uploadDate, duration, url, isFamilyFriendly, genre, keywords, contentSize, videoQuality, commentCount } = json.result
-  let { name: authorname, url: authorlink } = author
-  let dateConfig = {
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-  }
-  let unknown = '_Unknown_'
-  let none = '_None_'
-  let caption = `
-Konten${isFamilyFriendly ? ' ' : ' *Tidak* '}Family Friendly
-Post oleh ${name} (${authorname || ''}) (${authorlink || ''})
-Diposting pada ${new Date(uploadDate).toLocaleDateString('id', dateConfig)}
-Size: ${contentSize || unknown}
-Durasi: ${clockString(+ new Date(duration))}
-Genre: ${genre || none}
-Kualitas: ${quality ? quality : unknown}
+let handler = async(m, { conn, text }) => {
+    let [link, resolusi] = text.split `|`
 
-${description}
+    if (!link) return conn.reply(m.chat, 'Uhm... urlnya mana?', m)
+    if (!resolusi) return conn.reply(m.chat, 'Harap memasukkan resolusi hd/sd !', m)
 
-Keyword: ${keywords || none}
-`.trim()
-  conn.sendFile(m.chat, url, 'media-fb', caption, m)
+    conn.reply(m.chat, 'Searching...', m)
+    new Promise((resolve, reject) => {
+        axios.get(`https://mnazria.herokuapp.com/api/fbdownloadervideo?url=` + encodeURIComponent(link))
+            .then((res) => {
+                if (resolusi == 'hd') {
+                    let dl_link = res.data.resultHD
+                } else {
+                    let dl_link = res.data.resultSD
+                }
+                conn.reply(m.chat, `*Link:* ${dl_link} \n\nfile akan segera dikirim!`, m)
+                conn.sendFile(m.chat, dl_link, 'video.mp4', `*Title:* ${text}\n*Link:* ${dl_link}`, m)
+
+            })
+            .catch(reject => {
+                conn.reply(m.chat, 'Ada yang Erorr!', m)
+            })
+    })
+
 }
-handler.help = ['fb'].map(v => v + ' <url>')
-handler.tags = ['downloader']
 
-handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
+handler.help = ['fb <hd/sd|url>']
+handler.tags = ['downloader']
+handler.command = /^fb$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = true
 
 module.exports = handler
-
-function clockString(ms) {
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
-}
