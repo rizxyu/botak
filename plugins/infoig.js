a4d04a3ca1098903b9cf95595f3781244b647624
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-04 20:13:14 +0700

    Update infoig.js

--- plugins/infoig.js
+++ plugins/infoig.js
@@ -5,6 +5,7 @@ let handler = async(m, { conn, text }) => {
      axios.get(`https://alfians-api.herokuapp.com/api/stalk?username=${teks}`).then ((res) =>{
   let hasil = `BIODATA INSTAGRAM ATAS NAMA _${teks}_ \n\n *Username* : _${res.data.Username}_ \n *Nama* : _${res.data.Name}_ \n *Jumlah Followers* : _${res.data.Jumlah_Followers}_ \n *Jumlah Following* : _${res.data.Jumlah_Following}_ \n *Jumlah Post* : _${res.data.Jumlah_Post}_ `
   conn.sendMessage(id, hasil, MessageType.text)
+   }
 }
 handler.help = ['infoig']
 handler.tags = ['internet']
