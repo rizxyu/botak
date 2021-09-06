f615793051e1e3a9c82929d9088d11d33d54132b
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-06 16:16:52 +0700

    Update tebakumur.js

--- plugins/tebakumur.js
+++ plugins/tebakumur.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, 'Masukan Teksnya', m)
 
     await m.reply('Searching...')
-	axios.get(`http://lolhuman.herokuapp.com/api/tebakumur?apikey=31caf10e4a64e86c1a92bcba&name=${text}`).then ((res) => {
+	axios.get(`http://lolhuman.herokuapp.com/api/tebakumur?apikey=HIRO&name=${text}`).then ((res) => {
 	 	let hasil = `Namamu : ${text}\nUmurmu : ${res.data.result.age}`
 
     conn.reply(m.chat, hasil, m)
