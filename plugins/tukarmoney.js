cfb82465f68faa4f7fb0abe3b270f0df8ad755ef
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-06 17:37:23 +0700

    Update tukarmoney.js

--- plugins/tukarmoney.js
+++ plugins/tukarmoney.js
@@ -3,14 +3,19 @@ let hmm = 100
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `_Example:_ ${usedPrefix + command} 10`
     let user = global.DATABASE.data.users[m.sender]
-    user.exp -= text
-    user.money += hmm * text
-    
+   
+     
     let caption = `
 Kamu Menukarkan 🎑Exp kamu dengan money senilai ${text * hmm}money💵`
 
 
+
+if ( user.exp >= text) {
+    user.exp -= text
+    user.money += hmm * text
+ 
     conn.sendButton(m.chat, caption, '©RainXyz','Tukar Lagi',`${usedPrefix + command} 10`
+  } else m.reply('ngadi Ngadi lu duit abis mau ditukerin apa')
     )
    }
 handler.help = ['tukarmoney <jumlah>']
