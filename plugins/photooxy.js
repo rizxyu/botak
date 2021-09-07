155f16cb770d8714f728ca250a44353100da15d6
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-07 12:59:37 +0700

    Add files via upload

--- plugins/photooxy.js
+++ plugins/photooxy.js
@@ -5,30 +5,31 @@
 
 const fetch = require('node-fetch')
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+let handler = async (m, { conn, text, usedPrefix, command }) => {
 
 let apikey = 'HIRO'//BELI SENDIRI NGENTOT
 
 let caption = `[ ❗ ] Sudah Jadi Nih we`
 let wait = `await`
 
+
 //APIKEY WEB
-let res = `https://api.lolhuman.xyz/api/photooxy1/shadow?apikey=${apikey}&text=${args[0]}`
-let lov = `https://api.lolhuman.xyz/api/photooxy1/shadow?apikey=${apikey}&text=${args[0]}`
-let wud = `https://api.lolhuman.xyz/api/photooxy1/woodheart?apikey=${apikey}&text=${args[0]}`
-let cu = `https://api.lolhuman.xyz/api/photooxy1/cup?apikey=${apikey}&text=${args[0]}`
-let cu2 = `https://api.lolhuman.xyz/api/photooxy1/cup2?apikey=${apikey}&text=${args[0]}`
-let kopi = `https://api.lolhuman.xyz/api/photooxy1/coffe?apikey=${apikey}&text=${args[0]}`
+let res = `https://api.lolhuman.xyz/api/photooxy1/shadow?apikey=${apikey}&text=${text}`
+let lov = `https://api.lolhuman.xyz/api/photooxy1/love?apikey=${apikey}&text=${text}`
+let wud = `https://api.lolhuman.xyz/api/photooxy1/woodheart?apikey=${apikey}&text=${text}`
+let cu = `https://api.lolhuman.xyz/api/photooxy1/cup?apikey=${apikey}&text=${text}`
+let cu2 = `https://api.lolhuman.xyz/api/photooxy1/cup2?apikey=${apikey}&text=${text}`
+let kopi = `https://api.lolhuman.xyz/api/photooxy1/coffe?apikey=${apikey}&text=${text}`
      
- 
+ //FUNCTION SEND FILENYA CUK
 try {
 conn.reply( m.chat, wait, m)
-      if (/shadow/.test(command)) return conn.sendFile( m.chat, res, caption, m)
-      if (/love/.test(command)) return conn.sendFile( m.chat, lov, caption, m)
-     if (/woodheart/.test(command)) return conn.sendFile( m.chat, wud, caption, m)
-     if (/cup/.test(command)) return conn.sendFile( m.chat, cu, caption, m)
-     if (/cup2/.test(command)) return conn.sendFile( m.chat, cu2, caption, m)
-     if (/coffe/.test(command)) return conn.sendFile( m.chat, kopi, caption, m)
+      if (/shadow/.test(command)) conn.sendFile( m.chat, res, caption, m)
+      if (/love/.test(command)) conn.sendFile( m.chat, lov, caption, m)
+     if (/woodheart/.test(command))  conn.sendFile( m.chat, wud, caption, m)
+     if (/cup/.test(command))  conn.sendFile( m.chat, cu, caption, m)
+     if (/cup2/.test(command))  conn.sendFile( m.chat, cu2, caption, m)
+     if (/coffe/.test(command)) conn.sendFile( m.chat, kopi, caption, m)
      
 } catch (e) {
    throw e
