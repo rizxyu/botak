2e6d39ba012e2be56d3286a77636dd0517f46b62
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-08 15:50:35 +0700

    Add files via upload

---
+++ plugins/jadwalsolat.js
@@ -0,0 +1,27 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, text }) => {
+
+let res = await fetch(`https://api-zeeoneofc.herokuapp.com/api/jadwalshalat?kota=${text}&apikey=Alphabot`)
+json = await res.json()
+if (!json.result) 
+
+let caption = `
+${json.result.tanggal}
+
+Imsyak : ${json.result.imsyak}
+Shubuh : ${json.result.shubuh}
+Dzuhur : ${json.result.dzuhur}
+Ashar : ${json.result.ashr}
+Maghrib : ${json.result.magrib}
+Isya : ${json.result.isya}
+`
+conn.reply( m.chat, caption, m)
+else conn.sendButton( m.chat, 'Sepertinya ada yg eror', 'Eror404', 'Report', '.bug ayatkursi')
+}
+
+handler.help = ['jadwalsolat <daerah>']
+handler.tags = ['quran']
+handler.command = /^(jadwalsholat|jadwalsolat)$/i
+
+module.exports = handler
+
---
+++ plugins/kisahnabi.js
@@ -0,0 +1,18 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, text }) => {
+
+if (!text) throw '*Contoh:* /kisahnabi muhammad'
+
+let res = await fetch(`https://api-zeeoneofc.herokuapp.com/api/kisahnabi?nabi=${text}&apikey=Alphabot`)
+json = await res.json()
+if (!json.result.nabi) 
+conn.sendFile( m.chat, json.result.nabi.image, 'kisahnabi.jpg', `*KISAH NABI*\n*${json.result.nabi.nabi}*\n===============\n\n*lahir:* ${json.result.nabi.lahir} \n*Umur:* ${json.result.nabi.umur}\n*Tempat Kelahiran:* ${json.result.nabi.tempat}\n*📑Kisah:* ${json.result.nabi.kisah}\n`, m)
+else conn.sendButton( m.chat, 'Sepertinya ada yg eror', 'Eror404', 'Report', '.bug ayatkursi')
+}
+
+handler.help = ['kisahnabi <namanabi>']
+handler.tags = ['quran']
+handler.command = /^(kisahnabi)$/i
+
+module.exports = handler
+
