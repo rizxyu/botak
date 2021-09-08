0840297f96495f640fbea0d1f14f46dd5e184cb4
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-08 21:48:16 +0700

    Add files via upload

--- plugins/rankcard.js
+++ plugins/rankcard.js
@@ -1,5 +1,5 @@
 let levelling = require('../lib/levelling')
-let fetch = require('node-fetch')
+///let fetch = require('node-fetch')
 let fs = require('fs')
 
 let handler  = async (m, { conn, text }) => {
@@ -7,7 +7,7 @@ let handler  = async (m, { conn, text }) => {
 	
       let pp = await conn.getProfilePicture(m.sender)
 let { exp, limit, level, role } = global.DATABASE.data.users[m.sender]
-    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
+let { min, xp, max } = levelling.xpRange(level, global.multiplier)
 let name = conn.getName(m.sender)
 let res = `http://hardianto-chan.herokuapp.com/api/rankcard?profile=${pp}&name=${name}&bg=https://i.ibb.co/y4S494f/wallpaper.jpg&needxp=${max}&curxp=${exp}&level=${level}&logorank=https://i.ibb.co/Wn9cvnv/FABLED.png`
 let caption = `
