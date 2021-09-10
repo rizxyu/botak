fdbb5c337ce09540e940110436f7cb9df2e65587
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-10 13:56:47 +0700

    Add files via upload

--- plugins/kick.js
+++ plugins/kick.js
@@ -1,9 +1,19 @@
 let { WAConnection: _WAConnection, WA_MESSAGE_STUB_TYPES } = require('@adiwajshing/baileys')
-let handler = async (m, { conn, args }) => {
-  let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
-  let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
-  for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
+
+let handler = async(m, {conn, usedPrefix, args, command}) => {
+
+let user = global.DATABASE.data.users[m.sender]
+
+if (!users || args[0]) throw 'Tag Member to kick'
+let users = m.mentionedJid ? m.mentionedJid[0] : (args[0].replace(/[@ .+-]/g, '').replace(' ', '') + '@s.whatsapp.net')
+for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
+await conn.sendMessage( m.chat, `Succes Kick ${args}`, m, false, {
+    contextInfo: {
+      mentionedJid: conn.parseMention(text)
+    }
+  })
 }
+
 handler.help = ['kick', '-'].map(v => v + ' @user')
 handler.tags = ['admin']
 handler.command = /^(kick|\-)$/i
