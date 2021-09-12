1ec47dd2a2e8205a5c51d65d153cf2753f9f21d9
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-12 18:23:37 +0700

    Update Asalam.js

--- plugins/Asalam.js
+++ plugins/Asalam.js
@@ -1,7 +1,6 @@
 let handler = async (m, { conn }) => {
+let caption = `*Waalaikummussalam warahmatullahi wabarokatuh*
 
-let caption = `
-*Waalaikummussalam warahmatullahi wabarokatuh*
 
 _📚 Baca yang dibawah ya!_
 "Orang yang mengucapkan salam seperti ini maka ia mendapatkan 30 pahala, kemudian, orang yang dihadapan atau mendengarnya membalas dengan kalimat yang sama yaitu “Wa'alaikum salam warahmatullahi wabarakatuh” atau ditambah dengan yang lain (waridhwaana). Artinya selain daripada do'a selamat juga meminta pada Allah SWT"
