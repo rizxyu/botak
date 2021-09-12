011c73ac35dc68b798480353b43455576b761145
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-12 18:52:59 +0700

    Create _antikotor.js

---
+++ plugins/_antikotor.js
@@ -0,0 +1,22 @@
+let handler = m => m
+
+let bhsKotor = /(a(su|njeng)kontol|kntl||bngst|anjime|anjim|pepek|memek|anjg|pantek)/i
+handler.before = function (m, { user }) {
+  if (m.isBaileys && m.fromMe ) return true
+  let chat = global.DATABASE.data.chats[m.sender]
+  let grup = bhsKotor.exec(m.text)
+  let botol = global.botwm
+
+if (chat.antiToxic && grup ) {
+
+conn.sendButton( m.chat, `*[ ANTI BADWORD ]*
+
+_JANGAN TOXIC BANG DOSA!_
+`, `${botol}`, `Astaghfirullah`, `p`, m)
+ if (global.opts['restrict']) {
+        }
+    }
+ return true
+}
+
+modular.exports = handler
