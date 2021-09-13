5c6cdd3d317c280ddc2b6216137d63da738af74e
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-13 09:18:50 +0700

    Update menu1.js

--- plugins/menu1.js
+++ plugins/menu1.js
@@ -156,8 +156,8 @@ if (teks == 'randomimage') tags = {
     let { exp, limit, level, role, registered } = global.DATABASE.data.users[m.sender]
     let { min, xp, max } = levelling.xpRange(level, global.multiplier)
     let name = registered ? global.DATABASE.data.users[m.sender].name : conn.getName(m.sender)
-    let user = global.DATABASE.data.users[m.sender]
-    let avtar = await conn.getProfilePicture(conn.user.jid)
+    //let user = global.DATABASE.data.users[m.sender]
+    //let avtar = await conn.getProfilePicture(conn.user.jid)
     let d = new Date(new Date + 3600000)
     let locale = 'id'
     // d.getTimeZoneOffset()
@@ -423,7 +423,7 @@ _orang orang apa yg ngeselin_
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    conn.send2ButtonImg(m.chat, text.trim(), avtar, `•Rain Xyz•`, `Owner`,`.owner`, `Donasi`, `.donasi`, m)
+    conn.send2Button(m.chat, text.trim(), `•Rain Xyz•`, `Owner`,`.owner`, `Donasi`, `.donasi`, m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
