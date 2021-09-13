e47fe46a1d4cf70dbd97e90b221984411f04881b
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-13 18:42:21 +0700

    Update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   let json = await res.json()
   if (!json.status) throw json
   let pint = json.data[Math.floor(Math.random() * json.data.length)];
-  conn.sendFile(m.chat, pint, '', '© stikerin', m, 0, { thumbnail: await (await fetch(pint)).buffer() })
+  conn.sendButtonImg(m.chat, `Sukses mencari ${text}` , pint, `${botol}`, `LAGI`, `.pinterest ${text}`, m, 0, { thumbnail: await (await fetch(pint)).buffer() })
 }
 handler.help = ['pinterest <pencarian>']
 handler.tags = ['internet']
