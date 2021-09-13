dd3aeb4326de246237899626f93e43edf182c7d4
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-13 19:01:16 +0700

    Create Buttonfitur.js

---
+++ plugins/Buttonfitur.js
@@ -0,0 +1,13 @@
+let rizxyu = async (m, { conn, command }) => {
+
+try {
+   switch (command) {
+case 'tes':
+m.reply('work')
+break
+     } catch (e) {
+    throw e
+     }
+}
+
+module.exports = rizxyu
