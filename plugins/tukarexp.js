be89f40301f534619f405ab24c3317975841a17b
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-13 18:35:52 +0700

    Update tukarexp.js

--- plugins/tukarexp.js
+++ plugins/tukarexp.js
@@ -1,4 +1,4 @@
-let hmm = 100
+
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `_Example:_ ${usedPrefix + command} 10`
@@ -6,16 +6,16 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
    
      
     let caption = `
-Kamu Menukarkan 💵Uang kamu dengan exp senilai *${text * hmm}* money💵`
+Kamu Menukarkan Exp kamu dengan Uang senilai Rp*${text * hmm}*💵`
 
 
 
 if ( user.money >= text) {
     user.money -= text
-    user.exp += hmm * text
+    user.exp += text
  
     conn.sendButton(m.chat, caption, '©RainXyz','Tukar Lagi',`${usedPrefix + command} 10`
-   )
+   , m)
  } else m.reply('ngadi Ngadi lu duit abis mau ditukerin apa')
     
    }
