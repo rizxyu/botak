a0567c2a73a9bf419a91f21ac8e3c35c3a483c96
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-14 18:20:55 +0700

    Update Duel.js

--- plugins/Duel.js
+++ plugins/Duel.js
@@ -14,15 +14,17 @@ let user = global.DATABASE.data.users[m.sender]
 
 let count = args[1] && args[1].length > 0 ? Math.min(100, Math.max(parseInt(args[1]), 1)) : Math.min(1)
 
+let nama = conn.getName[m.sender]
+
      try {
+
    if (/duel/.test(command)) {
    if (!who || !args[0]) return m.reply('tag yg ingin di ajak duel!')
-   conn.send2Button( m.chat, ` @${m.sender.replace(/@.+/, '')} Mengajak duel ${args[0]}\n\nPilih Y Atau No`, `Games wabot`, `Ya`, `.dya`, `No`, `.dno`, m, false, {
-    contextInfo: {
-      mentionedJid: conn.parseMention(args)
-    }
-  })
+   conn.send2Button(m.chat, ` ${nama} Mengajak duel ${args[0]}\n\nPilih Y Atau No`, `Games wabot`, `Ya`, `.dya`, `No`, `.dno`, m)
            }
+   if (/dya/.test(command)) {
+   m.reply(`mbuh`)
+   }
        } catch (e) {
         return conn.sendButton( m.chat, `Sepertinya ada bug`, `laporkan ke owner`, `Kanjut Badag`, `.bug eror duel`, m)
          }
