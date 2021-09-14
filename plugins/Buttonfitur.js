0002a34e258daaaba62720dc63166a30d5f83fa1
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-14 12:25:10 +0700

    Update Buttonfitur.js

--- plugins/Buttonfitur.js
+++ plugins/Buttonfitur.js
@@ -1,14 +1,16 @@
 const fbdl = require("fbdl-core");
 const fs = require('fs')
+const { MessageType } = require('@adiwajshing/baileys')
+const { getBuffer } = require('./lib/functions.js')
 
 let handler = async ( m, { conn, command}) => {
 
 fbdl.download("https://www.facebook.com/alanwalkermusic/videos/277641643524720")
     .then(res => {
-        res.pipe(fs.createWriteStream("./aw.mp4"));
+        res.pipe(getBuffer.fs.createWriteStream("./aw.mp4"));
     });
 
-await conn.sendMessage(m.chat, fs.readFileSync("./aw.mp4"), mediaType.video)
+await conn.sendMessage(m.chat, getBuffer.fs.readFileSync("./aw.mp4"), MessageType.video)
 
    }
 
