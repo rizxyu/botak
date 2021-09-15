44c234c5fd6dd3044fa072e2426aa757e6f3ee29
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-15 20:57:35 +0700

    Update Duel.js

--- plugins/Duel.js
+++ plugins/Duel.js
@@ -19,10 +19,14 @@ let handler = async ( m, { conn, args, command}) => {
   let randomkamu = `${Math.floor(Math.random() * 81)}`.trim()
   let Aku = (randomaku * 1)
   let Kamu = (randomkamu * 1)
-
+  let __timers = (new Date - global.DATABASE._data.users[m.sender].lastduel)
+  let _timers = (300000 - __timers) 
+  let timers = clockString(_timers)
    try {
      if (/duel/.test(command)) {
        if (!who) return m.reply('tag yg ingin di ajak duel!')
+       if (new Date - global.DATABASE._data.users[m.sender].lastduel > 300000) return conn.sendButton( m.chat, `Kamu sudah berlaga duel Tunggu hingga\n ${timers}`, `games-botwa`,`Afk`, `.afk habis bergelud`, m )
+           
        conn.send2Button(m.chat, ` @${who.split("@")[0]} Mengajak duel ${args[0]}\n\nPilih Y Atau No`, `Games wabot`, `Ya`, `+dya`, `No`, `+dno`, m)
      }
 
@@ -30,6 +34,7 @@ let handler = async ( m, { conn, args, command}) => {
      let kenal = !who.includes(m.sender)
      if(kenal) throw 'Lu siapa?\nkok ikut kut mau duel'
      delete conn.duel
+     global.DATABASE._data.users[m.sender].lastduel = new Date * 1
      if (Aku > Kamu) {
        user.money -= 900
        enemy.money += 900
@@ -37,7 +42,7 @@ let handler = async ( m, { conn, args, command}) => {
      } else if (Aku < Kamu) {
        user.money += 900
        enemy.money -= 900
-       conn.reply(m.chat, `@${who.split("@")[0]} Kalah\n*Hadiah:*\n 900 money`.trim(), m)
+       conn.reply(m.chat, `@${who.split("@")[0]} Kalah\n*Hadiah:*\n 450 money`.trim(), m)
      } else {
        user.money += 450
        enemy.money += 450
@@ -59,3 +64,14 @@ handler.tags = ['rpg']
 handler.command = /^(duel|dya|dno)/i
 
 module.exports = handler
+
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)]
+}
+function clockString(ms) {
+  let h = Math.floor(ms / 3600000)
+  let m = Math.floor(ms / 60000) % 60
+  let s = Math.floor(ms / 1000) % 60
+  console.log({ms,h,m,s})
+  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
+}
