a5fc393eb073a90cfd34c94540eda4c116bd6065
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-16 06:28:08 +0700

    Update Duel.js

--- plugins/Duel.js
+++ plugins/Duel.js
@@ -21,13 +21,13 @@ let handler = async ( m, { conn, args, command}) => {
   let Kamu = (randomkamu * 1)
 
   let __timers = (new Date - user.lastduel)
-  let _timers = (300000 - __timers) 
+  let _timers = (3000000 - __timers) 
   let timers = clockString(_timers)
 
    try {
      if (/duel/.test(command)) {
        if (!who) return m.reply('tag yg ingin di ajak duel!')
-       if (new Date - user.lastduel > 300000) return conn.sendButton( m.chat, `Kamu sudah berlaga duel Tunggu hingga\n ${timers}`, `games-botwa`,`Afk`, `.afk habis bergelud`, m )
+       if (new Date - user.lastduel > 3000000) return conn.sendButton( m.chat, `Kamu sudah berlaga duel Tunggu hingga\n ${timers}`, `games-botwa`,`Afk`, `.afk habis bergelud`, m )
        conn.send2Button(m.chat, ` @${who.split("@")[0]} Mengajak duel ${args[0]}\n\nPilih Y Atau No`, `Games wabot`, `Ya`, `+dya`, `No`, `+dno`, m)
      user.lastduel = new Date * 1
      }
