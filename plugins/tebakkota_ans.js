2d97c27997afb294591e4a9df82f66aabac28d32
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-16 17:23:56 +0700

    Update tebakkota_ans.js

--- plugins/tebakkota_ans.js
+++ plugins/tebakkota_ans.js
@@ -7,12 +7,12 @@ handler.before = async function (m) {
     if (m.quoted.id == conn.tebakkota[id][0].id) {
         let json = JSON.parse(JSON.stringify(conn.tebakkota[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
-        if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
+        if (m.text.toLowerCase() == json.result.jawaban.toLowerCase().trim()) {
             global.DATABASE.data.users[m.sender].money += conn.tebakkota[id][2]
             m.reply(`*Benar!*\n+${conn.tebakkota[id][2]} XP`)
             clearTimeout(conn.tebakkota[id][3])
             delete conn.tebakkota[id]
-        } else if (m.text.toLowerCase().endsWith(json.name.split` `[1])) m.reply(`*Dikit Lagi!*`)
+        } else if (m.text.toLowerCase().endsWith(json.result.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
         else m.reply(`*Salah!*`)
     }
     return !0
