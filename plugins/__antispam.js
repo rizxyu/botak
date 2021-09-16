1f9df8b5c45c49beb975140f9e26bad2d8dfe6ab
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-16 13:47:28 +0700

    Update __antispam.js

--- plugins/__antispam.js
+++ plugins/__antispam.js
@@ -12,14 +12,15 @@ handler.all = async function (m) {
         this.spam[spaming.jid] = spaming
     } else try {
         this.spam[m.sender].spam += 1
-        this.spam[m.sender].Warn += 1
+        global.DATABASE.data.users[m.sender].warn += 1
+        let matot = global.DATABASE.data.users[m.sender].warn
         let name = this.getName(m.sender)
         if (new Date - this.spam[m.sender].lastspam > 4000) {
             if (this.spam[m.sender].spam > 6) {
                 this.spam[m.sender].spam = 0
                 this.spam[m.sender].lastspam = new Date * 1
                // global.DATABASE._data.users[m.sender].banned = true
-                m.reply(`*[ ANTI SPAM ]*\n*${name}* Kamu Telah Melakukan spam\n Jika melebihi 5 warn maka akan terbanned`)
+                m.reply(`*[ ANTI SPAM ]*\n\n*${name}* Kamu Telah Melakukan spam\n*⚠️Warn:* (${matot}/5)\n_Jika melebihi 5 warn maka akan terbanned_`)
             } else {
                 this.spam[m.sender].spam = 0
                 this.spam[m.sender].lastspam = new Date * 1
