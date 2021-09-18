711daefa4e4679302e57a582212b0e81dd9a42a1
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-18 19:32:25 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,11 +2,11 @@ let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
 │ • Telkomsel [ 082328303332 ]
 │ • OVO [ 082328303332 ]
-    • Dana [ 082328303332 ]
-    • Paypal [ 6282328303332 ]
+│   • Dana [ 082328303332 ]
+│   • Paypal [ 6282328303332 ]
 ╰────
-╭─「 Hubungi 」
-│ > Ingin donasi? Wa.me/6282328303332
+╭─「 NOTE 」
+│ > KASIH BUKTI PEMBAYARAN KE OWNER Y
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
