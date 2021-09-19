ae07559842cedb9defb3cbf31d33cbd596006201
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-19 17:06:51 +0700

    Add files via upload

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -6,14 +6,14 @@ handler.all = async function (m) {
     if (m.chat.endsWith('broadcast')) return
     if (m.fromMe) return
     if (m.isGroup) return
-    if (DATABASE.data.settings.groupOnly) return
-    let user = global.DATABASE.data.users[m.sender]
+    if (db.data.settings.groupOnly) return
+    let user = global.db.data.users[m.sender]
     if (new Date - user.pc < 86400000) return // setiap 24 jam sekali
     await this.sendButton(m.chat, `
 Hai, ${ucapan()}
 
 ${user.banned ? 'kamu dibanned' : 'Ada yang bisa saya bantu?'}
-`.trim(), '© RainXyz', user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : ',menu')
+`.trim(), '© stikerin', user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : ',?', m)
     user.pc = new Date * 1
 }
 
