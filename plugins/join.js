57f24504e260a070a329899d73d8f92f70eb4a56
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-21 11:29:14 +0700

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -5,10 +5,13 @@ let handler = async (m, { conn, text, isMods, isOwner }) => {
     let [_, code] = link.match(linkRegex) || []
     if (!code) throw 'Link invalid'
     if (isMods || isOwner || m.fromMe) {
+        let __timers = (new Date - global.DATABASE.data.chats[res.gid].expired)
+        let _timers = (86400000 - __timers)
+        let timers = clockString(_timers) 
         let res = await conn.acceptInvite(code)
-        global.DATABASE.data.chats[res.gid].expired += 3600
+        global.DATABASE.data.chats[res.gid].expired += new Date * 1
         let gray = global.DATABASE.data.chats[res.gid].expired
-        m.reply(`Berhasil join grup ${res.gid}\n${gray}`)
+        m.reply(`Berhasil join grup ${res.gid}\n berakhir dalam ${gray}`)
     } else {
         for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)) m.reply('*dari:* ' + m.sender.split('@')[0] + '\n*Link:* ' + link, jid)
         m.reply('Sedang di process Owner')
@@ -20,3 +23,11 @@ handler.tags = ['premium']
 handler.command = /^join$/i
 
 module.exports = handler
+
+function clockString(ms) {
+  let h = Math.floor(ms / 3600000)
+  let m = Math.floor(ms / 60000) % 60
+  let s = Math.floor(ms / 1000) % 60
+  console.log({ms,h,m,s})
+  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
+}
