9cf182a1fb7a470cebb91ca502ea4033cc410ccc
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-21 11:32:26 +0700

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -1,33 +1,25 @@
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 
-let handler = async (m, { conn, text, isMods, isOwner }) => {
-    let link = (m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text
-    let [_, code] = link.match(linkRegex) || []
-    if (!code) throw 'Link invalid'
-    if (isMods || isOwner || m.fromMe) {
-        let __timers = (new Date - global.DATABASE.data.chats[res.gid].expired)
-        let _timers = (86400000 - __timers)
-        let timers = clockString(_timers) 
-        let res = await conn.acceptInvite(code)
-        global.DATABASE.data.chats[res.gid].expired += new Date * 1
-        let gray = global.DATABASE.data.chats[res.gid].expired
-        m.reply(`Berhasil join grup ${res.gid}\n berakhir dalam ${gray}`)
-    } else {
-        for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)) m.reply('*dari:* ' + m.sender.split('@')[0] + '\n*Link:* ' + link, jid)
-        m.reply('Sedang di process Owner')
-    }
+let handler = async (m, { conn, text, usedPrefix }) => {
+    let [_, code] = text.match(linkRegex) || []
+    if (!code) throw 'Link Salah'
+    let res = await conn.acceptInvite(code)
+    m.reply(`Berhasil join grup ${res.gid}`).then(() => {
+        var jumlahHari = 86400000 * 0.5
+        var now = new Date() * 1
+        if (now < global.DATABASE.data.chats[res.gid].expired) global.DATABASE.data.chats[res.gid].expired += jumlahHari
+        else global.DATABASE.data.chats[res.gid].expired = now + jumlahHari
+    })
+    await conn.sendButton(m.chat, `
+*${conn.user.name}* adalah bot whatsapp yang dibangun dengan Nodejs, *${conn.user.name}* diundang oleh @${m.sender.split`@`[0]}
+    
+ketik *${usedPrefix}menu* untuk melihat daftar perintah`, '© stikerin', 'Menu', `${usedPrefix}?`, { contextInfo: { mentionedJid: [m.sender] } })
 }
-handler.help = ['join [chat.whatsapp.com]']
-handler.tags = ['premium']
+handler.help = ['join <chat.whatsapp.com>']
+handler.tags = ['']
 
 handler.command = /^join$/i
 
-module.exports = handler
+handler.premium = false
 
-function clockString(ms) {
-  let h = Math.floor(ms / 3600000)
-  let m = Math.floor(ms / 60000) % 60
-  let s = Math.floor(ms / 1000) % 60
-  console.log({ms,h,m,s})
-  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
-}
+module.exports = handler
