66cb098fb9cdf040417d07d06d9daf5146d467e2
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-25 15:29:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.packname = 'Rain Bot By Rizxyu\nDonate Me plis'
 global.author = ''
-global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=400&scaleHeight=200&fontsize=90&fillTextType=1&text='
 
 global.multiplier = 39 // The higher, The harder levelup
 
