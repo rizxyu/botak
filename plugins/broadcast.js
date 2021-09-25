fd94d729f564acfad8fc3817eba5e66195a87785
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-26 06:14:04 +0700

    Add files via upload

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -1,10 +1,14 @@
+let fetch = require ('node-fetch')
+
 let handler  = async (m, { conn, text }) => {
   let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
-  let content = (/bc|broadcast/i.test(text) ? text : text + '\n' + readMore + '「 ' + conn.getName(conn.user.jid) + ' Broadcast 」')
-  for (let id of chats) conn.sendMessage(id, content, m.mtype, m.msg.contextInfo ? {
-    contextInfo: m.msg.contextInfo
-  } : {})
-  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
+  let content = (/bc|broadcast/i.test(text) ? text : text + '\n' + readMore + '〔 ' + conn.getName(conn.user.jid) + ' Broadcast 〕')
+  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_\nestimasi selesai ${chats.length * 1.5} detik`, m)
+  for (let id of chats) {
+     await delay(1500)
+    await conn.sendButtonLoc(id, await ( await fetch( fla + 'broadcast')).buffer(), content, botwm, `Menu`, `/menu`, m)
+    }
+  m.reply('_*Broadcast Selesai*_')
 }
 handler.help = ['broadcast','bc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
@@ -25,3 +29,6 @@ module.exports = handler
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
+const randomID = length => require('crypto').randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
+
+const delay = time => new Promise(res => setTimeout(res, time))
