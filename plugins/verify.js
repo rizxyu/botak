bc7e05c1d5f57159c619e474b49f459c733ce8cf
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-25 20:04:47 +0700

    Update verify.js

--- plugins/verify.js
+++ plugins/verify.js
@@ -6,8 +6,6 @@ let fetch = require('node-fetch')
 let util = require('util')
 let { MessageType } = require('@adiwajshing/baileys')
 let { buttonsMessage, contactsArray, image, MimeType } = MessageType
-const uploadImage = require('./lib/uploadImage')
-
 
 let handler = async function (m, { text, conn, usedPrefix }) {
  let user = global.DATABASE.data.users[m.sender]
@@ -20,20 +18,14 @@ let handler = async function (m, { text, conn, usedPrefix }) {
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
 
-  let pp = './src/avatar_contact.png'
-  try {
-    pp = await uploadImage(await (await fetch(await this.getProfilePicture(user))).buffer())
-  } catch (e) {
-  } finally {
-  let verif = `https://hardianto.xyz/api/tools/verification?nama=${name}&namaGb=${name}&pepeGb=https://i.ibb.co/cDYstqX/Desktop-Wallpaper.jpg&sn=10&pepeUser=${pp}&bege=https://i.ibb.co/cDYstqX/Desktop-Wallpaper.jpg&apikey=hardianto`
-
-  conn.sendFile( m.chat, verif, 'pp.jpg',
+  
+  conn.reply( m.chat,
 `_Verify Succes [ ✅ ]_
 *🎐Name:* ${name}
 *🎐SN:* ${sn}
 
 Terimakasih karena sudah verifikasi`, m)
-   }
+   
 }
 handler.help = ['verify']
 handler.tags = ['main']
