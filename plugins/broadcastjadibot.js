c151975d57783382e98abd1830f20c5f8ef30d6e
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-26 12:54:32 +0700

    Update broadcastjadibot.js

--- plugins/broadcastjadibot.js
+++ plugins/broadcastjadibot.js
@@ -1,10 +1,15 @@
+let fetch = require ('node-fetch')
+
 let handler = async (m, { conn, text }) => {
   if (conn.user.jid === global.conn.user.jid) {
     let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
-    let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text + '\n' + readMore + '「 ' + conn.getName(conn.user.jid) + ' Broadcast 」')
-    for (let id of users) conn.copyNForward(id, content)
-    conn.reply(m.chat, `_Berhasil mengirim broadcast ke ${users.length} nomor yang jadi bot_
-${users.map(v => 'wa.me/' + v.replace(/[^0-9]/g,'') + '?text=.menu').join('\n')}`.trim(), m)
+    let content = (/bc|broadcast/i.test(text) ? text : text + '\n' + readMore + '「 ' + conn.getName(conn.user.jid) + ' Broadcast 」')
+    for (let id of users) {
+    await delay(1500)
+    await conn.send2ButtonLoc(id, await ( await fetch( fla + 'broadcast')).buffer(), content, botwm, `MENU`, `/menu`, `Donasi`, `/donasi`, m)
+    }
+    m.reply(`_Berhasil mengirim broadcast ke ${users.length} nomor yang jadi bot_
+${users.map(v => 'wa.me/' + v.replace(/[^0-9]/g,'') + '?text=.menu').join('\n')}`)
   } else conn.reply(m.chat, 'Fitur ini hanya untuk host bot',  m)
 }
 handler.help = ['broadcastjadibot','bcbot'].map(v => v + ' <teks>')
@@ -26,3 +31,4 @@ module.exports = handler
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
+const delay = time => new Promise(res => setTimeout(res, time))
