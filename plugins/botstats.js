98f7dc54625bb1bc70404c98fece297f76c2df00
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-26 10:28:20 +0700

    Add files via upload

---
+++ plugins/botstats.js
@@ -0,0 +1,94 @@
+let os = require('os')
+let util = require('util')
+let { performance } = require('perf_hooks')
+let { sizeFormatter } = require('human-readable')
+let format = sizeFormatter({
+  std: 'JEDEC', // 'SI' (default) | 'IEC' | 'JEDEC'
+  decimalPlaces: 2,
+  keepTrailingZeroes: false,
+  render: (literal, symbol) => `${literal} ${symbol}B`,
+})
+let handler = async (m, { conn }) => {
+     let _uptime = process.uptime() * 1000
+    let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+    let uptime = clockString(_uptime)
+    
+  const chats = conn.chats.all()
+  const groups = chats.filter(v => v.jid.endsWith('g.us'))
+  const groupsIn = groups.filter(v => !v.read_only)
+  const used = process.memoryUsage()
+  const cpus = os.cpus().map(cpu => {
+    cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+    return cpu
+  })
+  const cpu = cpus.reduce((last, cpu, _, { length }) => {
+    last.total += cpu.total
+    last.speed += cpu.speed / length
+    last.times.user += cpu.times.user
+    last.times.nice += cpu.times.nice
+    last.times.sys += cpu.times.sys
+    last.times.idle += cpu.times.idle
+    last.times.irq += cpu.times.irq
+    return last
+  }, {
+    speed: 0,
+    total: 0,
+    times: {
+      user: 0,
+      nice: 0,
+      sys: 0,
+      idle: 0,
+      irq: 0
+    }
+  })
+  let old = performance.now()
+  await m.reply('_Get Status..._')
+  let neww = performance.now()
+  let speed = neww - old
+  m.reply(`
+⏱️Runtime: ${uptime}
+🖲️Speed: ${speed} millidetik
+
+💬 Status :
+- *${groups.length}* Group Chats
+- *${groupsIn.length}* Groups Joined
+- *${groups.length - groupsIn.length}* Groups Left
+- *${chats.length - groups.length}* Personal Chats
+- *${chats.length}* Total Chats
+
+📱 *Phone Info* :
+${'```' + `
+🔋 Battery : ${conn.battery ? `${conn.battery.value}% ${conn.battery.live ? '🔌 Dicas...' : '⚡ tidak tercas'}` : 'Tidak dapat membaca Status battre'}
+${util.format(conn.user.phone)}
+`.trim() + '```'}
+
+💻 *Server Info* :
+RAM: ${format(os.totalmem() - os.freemem())} / ${format(os.totalmem())}
+
+ℹ️ Anti Bot
+Anti Call: ✔️
+Anti Spam: ✔️
+Anti Troli: ✔️
+`.trim())
+}
+handler.help = ['botstats', 'status', 'info']
+handler.tags = ['info']
+
+handler.command = /^(botstats|status|info)$/i
+module.exports = handler
+
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
