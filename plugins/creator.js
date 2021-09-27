5b820a705d799fc19fb280827408077843b1c6b7
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-27 10:25:48 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,8 +1,10 @@
+let ft = require('node-fetch')
+
 let { MessageType }= require('@adiwajshing/baileys')
 let { contactsArray } = MessageType
 let handler = function (m, { conn }) {
 this.sendContact(m.chat, '6282328303332', 'Owner', m)
-conn.reply(`INU CONTACT OWNER G USAH CHAT ANEH ANEH ANJG`)
+conn.sendButtonLoc(m.chat, await ( await ft(fla + 'Owner')).buffer(),`INU CONTACT OWNER G USAH CHAT ANEH ANEH AAMA MINTA SAVE`, botwm, `Menu`, `Donasi`,  m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
