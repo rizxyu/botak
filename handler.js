56c62a92800ce8c2ffd6acf188e6e63c38e5f2ab
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-27 19:02:51 +0700

    Add files via upload

--- handler.js
+++ handler.js
@@ -1,6 +1,4 @@
-/*
-* HEROKU KONTOL
-*/
+
 let fetch = require('node-fetch')
 let util = require('util')
 let simple = require('./lib/simple')
@@ -464,6 +462,7 @@ module.exports = {
 
       let isMods = isOwner || global.mods.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
       let isPrems = isROwner || global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
+      if (!isPrems && !m.isGroup && global.DATABASE._data.settings.groupOnly) return
       let groupMetadata = m.isGroup ? this.chats.get(m.chat).metadata || await this.groupMetadata(m.chat) : {} || {}
       let participants = m.isGroup ? groupMetadata.participants : [] || []
       let user = m.isGroup ? participants.find(u => u.jid == m.sender) : {} // User Data
