3986ee2b6dc3ae8989a5b3eb9de1b1bafed9f412
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-27 08:47:14 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,21 +1,8 @@
 let { MessageType }= require('@adiwajshing/baileys')
 let { contactsArray } = MessageType
 let handler = function (m, { conn }) {
-this.sendContact(m.chat, '6282328303332', 'Rizxyu', m)
-   conarray = []
-ownerContact = ['6288804280094','62822980698995','6285783417029','62823283033321','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0']
-  for (let i of ownerContact.map(v => v + '@s.whatsapp.net')) {
- vname = conn.contacts[i] != undefined ? conn.contacts[i].vname || conn.contacts[i].notify : undefined
-  conarray.push({
-"displayName": 'TEAM',
-"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:${vname ? `${vname}` : `${conn.user.name}`}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-})
-}
-conn.sendMessage(m.chat, {
-"displayName": `${conarray.length} kontak team`,
-"contacts": conarray 
-}, 'contactsArrayMessage', { quoted: m }
-conn.sendMessage(m.chat, 'Ini contact Team dan kontak owner\n\n*NOTE:* JANGAN CHAT YG MACEM MACEM KE OWNER APALAGI MINTA SAVE DAN TOXIC',MessageType.text, { quoted: m} )
+this.sendContact(m.chat, '6282328303332', 'Owner', m)
+conn.reply(`INU CONTACT OWNER G USAH CHAT ANEH ANEH ANJG`)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
