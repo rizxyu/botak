e7b12d2ce5ee38c1403f2fd2b256238a85a404aa
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-13 19:29:49 +0700

    Update buatGC.js

--- plugins/buatGC.js
+++ plugins/buatGC.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text }) => {
    
    if (!text) return m.reply('_Masukkan Nama Grup!_')
    try{
-         await m.reply(global.wait)
+         await m.reply(userbot.wait)
     let group = await conn.groupCreate(text, [m.sender])
     let link = await conn.groupInviteCode(group.gid)
     let url = 'https://chat.whatsapp.com/' + link;
